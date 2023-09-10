f20f8e78d3bb2feb4d56293629eac31c21a12e7d
FaroKlandee <f.klandee@gmail.com>
2023-09-10 10:16:11 +1000

    Initial commite
    Setup javafx environment
    started pointApp class

---
+++ pointApp/src/pointApp.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class pointApp extends Application {
+
+    // launch the application
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String args[]) {
+        // launch the application
+        launch(args);
+    }
+}
