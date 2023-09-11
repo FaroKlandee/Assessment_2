c678da30fcb17ed0bcbd4e95e74187274534936e
FaroKlandee <f.klandee@gmail.com>
2023-09-11 23:32:54 +1000

    Commit #5

--- fanSim/src/fanSimulator.java
+++ fanSim/src/fanSimulator.java
@@ -1,21 +1,26 @@
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Line;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 public class fanSimulator extends Application {
 
-    private static final int WIDTH = 400;
-    private static final int HEIGHT = 400;
-    private static final double FAN_RADIUS = 100;
-    private static final double BLADE_LENGTH = 80;
-    private static final double BLADE_WIDTH = 10;
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 800;
+    private static final double FAN_RADIUS = 200;
+    private static final double BLADE_LENGTH = 160;
+    private static final double BLADE_WIDTH = 20;
     private static final double ROTATE_SPEED = 1;
 
     private boolean isFanRunning = false;
@@ -27,42 +32,28 @@ public class fanSimulator extends Application {
     private Button reverseButton;
     private Button stopButton;
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Fan Simulator");
-
-        StackPane root = new StackPane();
-
-        Canvas canvas = new Canvas(WIDTH, HEIGHT);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        drawFan(gc);
-
-        startButton = new Button("Start");
-        reverseButton = new Button("Reverse");
-        stopButton = new Button("Stop");
-
-        startButton.setOnAction(e -> startFan());
-        reverseButton.setOnAction(e -> reverseFan());
-        stopButton.setOnAction(e -> stopFan());
-
-        root.getChildren().addAll(canvas, startButton, reverseButton, stopButton);
-
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
     private void drawFan(GraphicsContext gc) {
         gc.clearRect(0, 0, WIDTH, HEIGHT);
 
-        gc.setFill(Color.GRAY);
-        gc.fillOval(WIDTH / 2 - FAN_RADIUS, HEIGHT / 2 - FAN_RADIUS, FAN_RADIUS * 2, FAN_RADIUS * 2);
-
-        gc.setFill(Color.DARKGRAY);
+        // Inside your drawFan() method
         for (int i = 0; i < 4; i++) {
             double startAngle = 90 * i + bladeRotation;
+
+            gc.setFill(Color.IVORY);
+            gc.fillOval(WIDTH / 2 - FAN_RADIUS, HEIGHT / 2 - FAN_RADIUS, FAN_RADIUS * 2, FAN_RADIUS * 2);
+
+            // Apply a perspective transformation
+            Rotate rotation = new Rotate(startAngle, WIDTH / 2, HEIGHT / 2);
+            gc.setTransform(rotation.getMxx(), rotation.getMyx(), rotation.getMxy(), rotation.getMyy(),
+                    rotation.getTx(), rotation.getTy());
+
+            // Draw the fan blade
+            gc.setFill(Color.PURPLE);
             gc.fillArc(WIDTH / 2 - BLADE_LENGTH / 2, HEIGHT / 2 - BLADE_WIDTH / 2, BLADE_LENGTH, BLADE_WIDTH,
                     startAngle, 90, ArcType.ROUND);
+
+            // Reset the transformation
+            gc.setTransform(new Affine());
         }
     }
 
@@ -101,6 +92,44 @@ public class fanSimulator extends Application {
                 }
             }
         }).start();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Fan Simulator");
+
+        Line line = new Line(200, 200, 200, 350);
+        Rotate rotation = new Rotate();
+        rotation.pivotXProperty().bind(line.startXProperty());
+        rotation.pivotYProperty().bind(line.startYProperty());
+
+        line.getTransforms().add(rotation);
+
+        // Create a VBox to arrange buttons vertically at the top
+        HBox buttonBox = new HBox(10); // 10 is the spacing between buttons
+        buttonBox.setAlignment(Pos.BOTTOM_CENTER);
+
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawFan(gc);
+
+        startButton = new Button("Start");
+        reverseButton = new Button("Reverse");
+        stopButton = new Button("Stop");
+
+        startButton.setOnAction(e -> startFan());
+        reverseButton.setOnAction(e -> reverseFan());
+        stopButton.setOnAction(e -> stopFan());
+
+        // Add buttons to the VBox
+        buttonBox.getChildren().addAll(startButton, reverseButton, stopButton);
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(canvas, buttonBox);
+
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
